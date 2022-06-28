5c68561c11f933b4d86146b5ef8694e4588750f2
dandev <danmunteanu420@gmail.com>
2022-06-28 15:31:42 +0300

    run production

--- back-end/server.js
+++ back-end/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -13,13 +14,24 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
-app.get('/', (req, res) => {
-    return res.send('getting hello')
-})
+
+//serve the front end server because the production dont have client side
+if (process.end.NODE_ENV === 'production') {
+    //set build folder as static
+    app.use(express.static(path.join(__dirname, '../front-end/build')))
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'front-end', 'build', 'index.html'))
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: 'Welcome to the support desk API' })
+    })
+}
+
+
+app.use(errorHandler)
 
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
-app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
